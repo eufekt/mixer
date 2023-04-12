137478f3b9a998c7ac323240a0c7765ff39916af
Nichita Hariton <30513164+eufekt@users.noreply.github.com>
2023-04-12 15:10:08 -0400

    Add jest testing suite

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,8 +1,10 @@
 import { arenaBase } from "../config";
 
+
 export function buildChannelUrl(channel) {
   const userSlug = channel?.user?.slug;
   const channelSlug = channel?.slug;
+  if (userSlug === undefined || channelSlug === undefined) return arenaBase;
 
   return `${arenaBase}/${userSlug}/${channelSlug}`;
 }
@@ -16,7 +18,8 @@ export function massageUrl(url) {
 
 export function parseUsableBlocks(data) {
   let usableBlocks = [];
-  data.forEach((block) => {
+
+  data?.forEach((block) => {
     if (block.class === "Channel") {
       usableBlocks.push(block);
     } else if (block.class === "Media") {
