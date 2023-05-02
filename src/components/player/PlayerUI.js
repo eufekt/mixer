a2d47ce5cbbe2b43636be680f7a3e1448179f08c
Nichita Hariton <30513164+eufekt@users.noreply.github.com>
2023-05-02 18:50:15 -0400

    Allow user to connect current block

--- src/components/player/PlayerUI.js
+++ src/components/player/PlayerUI.js
@@ -2,7 +2,8 @@ import styles from "@/src/styles/PlayerUI.module.sass";
 import { Controls } from "./Controls";
 import Seek from "./Seek";
 import Status from "./Status";
-import { useEffect } from "react";
+import { ConnectModal } from "../explorer/Block/ConnectModal";
+import { useState } from "react";
 
 export function PlayerUI({
   handlePrev,
@@ -17,6 +18,7 @@ export function PlayerUI({
   currentBlock,
   url,
 }) {
+  const [showConnectModal, setShowConnectModal] = useState(false);
   return (
     <div className={styles.player}>
       <div className={styles.controls}>
@@ -39,11 +41,38 @@ export function PlayerUI({
         handleSeekChange={handleSeekChange}
       />
       {url && (
-        <a className={styles.a} href={url} target={"_blank"} rel="noreferrer">
+        <a href={url} target={"_blank"} rel="noreferrer">
           🔗
         </a>
       )}
+      {currentBlock && (
+        <div style={{ position: "relative" }}>
+          <div
+            onClick={() => setShowConnectModal(true)}
+            className={styles.connect}
+          >
+            connect {"\u2192"}
+          </div>
 
+          {showConnectModal && (
+            <div
+              style={{
+                height: 240,
+                width: 240,
+                position: "absolute",
+                right: 20,
+                top: 20,
+                border: "3px solid white",
+              }}
+            >
+              <ConnectModal
+                setShowConnectModal={setShowConnectModal}
+                block={currentBlock}
+              />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/player/Seek.js
+++ src/components/player/Seek.js
@@ -13,27 +13,27 @@ export default function Seek({
   const previewImage = block?.image?.square.url;
   return (
     <div className={styles.container}>
-      {block  && (
+      {block && (
         <>
-      <Preview image={previewImage} />
-        <div className={styles.seekContainer}>
-          <div className={styles.title}>{title}</div>
-          <input
-            type={"range"}
-            className={styles.seek}
-            style={{ width: "100%" }}
-            min={0}
-            max={0.999999}
-            step="any"
-            value={played}
-            onChange={handleSeekChange}
-            onMouseUp={handleSeekMouseUp}
-          />
-          <div className={styles.durations}>
-            <Duration seconds={duration * played} />
-            <Duration seconds={duration * (1 - played)} />
+          <Preview image={previewImage} />
+          <div className={styles.seekContainer}>
+            <div className={styles.title}>{title}</div>
+            <input
+              type={"range"}
+              className={styles.seek}
+              style={{ width: "100%" }}
+              min={0}
+              max={0.999999}
+              step="any"
+              value={played}
+              onChange={handleSeekChange}
+              onMouseUp={handleSeekMouseUp}
+            />
+            <div className={styles.durations}>
+              <Duration seconds={duration * played} />
+              <Duration seconds={duration * (1 - played)} />
+            </div>
           </div>
-        </div>
         </>
       )}
     </div>
